bd669736155614de6becc7bceb11a3a625223a35
Daila1 <90678721+Daila1@users.noreply.github.com>
2022-08-15 14:21:05 +0800

    Update App.py

--- App.py
+++ App.py
@@ -20,6 +20,6 @@ def index():
         return(render_template("index.html",result1=r1,result2=r2))
     else:
         return(render_template("index.html",result1="waiting",result2="waiting"))
-
+    
 if __name__ == "__main__":
-    app.run(host= "localhost",port = 5008,debug = False)
+    app.run(host= "localhost",port = 5009,debug = False)
